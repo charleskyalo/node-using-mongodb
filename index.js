c9c146bc840e344241d2b1b798b40c3d66c47b84
charles kyalo <charleskyalo689@gmail.com>
2021-02-05 23:31:13 +0300

    add mongoose and connect server to database

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
 );
 
-app.listen(PORT, () => 
+// adding mongoose connection
+let productsDb;
+const dbName = productsDb;
+const url = `mongodb://127.0.0.1:27017/${dbName}`
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.listen(PORT, () =>
     console.log(`Your server is running on port ${PORT}`)
 );
