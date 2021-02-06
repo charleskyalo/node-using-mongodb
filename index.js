d05e4fbf0b6300c1b298a62f6148e6016a6dafe3
charles kyalo <charleskyalo689@gmail.com>
2021-02-06 10:21:16 +0300

    remove error handling from mongoose connection

--- index.js
+++ index.js
@@ -15,17 +15,12 @@ app.get('/', (req, res) =>
 
 // adding mongoose connection
 
-const url = `mongodb://127.0.0.1:27017/productsDb`
-mongoose.connect(url, {
+mongoose.connect('mongodb://localhost/productsdb', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: "productsDb"
-}).then(() => {
-    console.log("database conncetion ready");
-}).catch(error => {
-    console.log(error);
-    throw new Error("No  connection to mongo detected");
 })
+
 
 app.listen(PORT, () =>
     console.log(`Your server is running on port ${PORT}`)
